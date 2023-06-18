9f168a2317cf54d61a752e26a3e0040fbeb56a3e
GeorgyShefer <132808044+GeorgyShefer@users.noreply.github.com>
2023-06-18 21:51:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿ class Program
+{
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            ILogger logger = new Logger();
+            int a = 4;
+            int b = 5;
+            ICalculate calculate = new Calculate();
+            logger.Log("Калькулятро исправен");
+            calculate.Sum(a, b);
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка {ex.Message}");
+            
+        }
+        
+    }
+}
+interface ICalculate
+{
+    int Sum(int a, int b);
+}
+class Calculate : ICalculate
+{
+    public int Sum(int a, int b)
+    {
+        int result = a + b;
+        Console.WriteLine(result);
+        return result;
+            
+    }
+}
+interface ILogger
+{
+    void Log(string message);
+}
+class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
